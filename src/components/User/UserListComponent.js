1f86c8b161f2277e9bbec1aa900c9961fa9d3068
Abdul Mobeen <mobeen@alkunat.com>
2020-02-19 11:39:17 +0300

    user-listing v2 && formatted files

--- src/components/User/UserListComponent.js
+++ src/components/User/UserListComponent.js
@@ -1,39 +1,45 @@
-import React from 'react';
-import { Button,
-    Media} from 'reactstrap';
-import USERS from '../../api/users';
+import React from "react";
+import { Button, Media } from "reactstrap";
+import USERS from "../../api/users";
 
-
-const UserList = (props=null) => {
-
- const userList = USERS.map((user) =>
-<Media key={user.id} className="media pt-3">
-            <Media left>
-            <Media object src='https://picsum.photos/75/75?random=1' alt={user.first_name} />
-            </Media>
-            <Media body className="ml-2">
-                 <Media heading>{user.first_name}</Media>
-                 <span class="d-block"> 
-            {user.city} | {user.gender}
-            <span class="text-right">
-
-            <Button color="primary" className="btn-sm">Add to Group</Button>
-                            
-                </span>
+const UserList = (props = null) => {
+  const userList = USERS.map(user => (
+    <Media key={user.id} className="media pt-3">
+      <Media left>
+        <Media
+          object
+          src={"http://localhost:3000/assets/images/profile-placeholder.jpg"}
+          width="75"
+          height="75"
+          alt={user.first_name}
+        />
+      </Media>
+      <Media body className="ml-2">
+        <Media heading>{user.first_name}</Media>
+        <span class="d-block">
+          {user.city} | {user.gender} |{" "}
+          {user.groups && user.groups.length ? (
+            <span>
+              Group joined<b>({user.groups.length})</b>
             </span>
-            </Media>
+          ) : (
+            <a href="#">Add to Group</a>
+          )}
+          <span class="text-right">
+            <Button
+              color="primary"
+              className="btn-sm"
+              style={{ float: "right", paddingRight: "5px" }}
+            >
+              View
+            </Button>
+          </span>
+        </span>
+      </Media>
+    </Media>
+  ));
 
-</Media>
-
-
-
-  );
-
-  return (
-   
-      <Media list>{userList}</Media>
-  );
-
-}
+  return <Media list>{userList}</Media>;
+};
 
 export default UserList;
